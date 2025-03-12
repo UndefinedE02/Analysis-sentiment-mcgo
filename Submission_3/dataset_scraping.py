3529b28f3547ad02ff1ccd6337c301052dab6d37
UndefinedE02 <climbingboys01@gmail.com>
2025-03-12 22:13:37 +0700

    Add files via upload

---
+++ Submission_3/dataset_scraping.py
@@ -0,0 +1,35 @@
+from google_play_scraper import reviews,Sort
+import pandas as pd
+
+# Nama package aplikasi di Play Store
+APP_ID = "com.mobilechess.gp"
+
+
+# Mengambil 5000 ulasan pertama
+all_reviews, _ = reviews(
+    app_id=APP_ID,
+    lang='id',  # Bahasa Indonesia
+    country='id',  # Negara Indonesia
+    sort=Sort.NEWEST,  # Mengambil ulasan terbaru terlebih dahulu
+    count=10000
+)
+
+# Memisahkan menjadi 5000 ulasan pertama dan 5000 ulasan terakhir
+reviews_first = all_reviews[:5000]
+reviews_last = all_reviews[-5000:]
+
+
+# Menggabungkan kedua dataset
+final_reviews = reviews_first + reviews_last
+
+
+data = pd.DataFrame(final_reviews)
+
+# Menampilkan nama kolom untuk pengecekan
+print("Kolom yang tersedia:", data.columns)
+
+# Memastikan hanya kolom yang tersedia yang disimpan ke CSV
+available_columns = [col for col in ['userName', 'score', 'content'] if col in data.columns]
+data[available_columns].to_csv("magic_chess_reviews.csv", index=False)
+
+print("Scraping selesai. Data disimpan dalam magic_chess_reviews.csv")
